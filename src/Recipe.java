04eef53940c60792d9ce7221921cfd86abc934a6
Djordje <djordjedjordjevic2@gmail.com>
2022-09-05 12:49:53 +0200

    Ponedeljka pocetak skaliranja

--- src/Recipe.java
+++ src/Recipe.java
@@ -1,20 +1,32 @@
+import java.util.ArrayList;
 import java.util.List;
 
-public class Recipe implements Priceable{
+public class Recipe implements Priceable {
 
     public String nazivRecepta;
     // enum
     public List<WeightedIngredient> ingrs;
+
+    public Recipe(String naziv) {
+        this.nazivRecepta = naziv;
+        this.ingrs = new ArrayList<>();
+    }
 
 
     @Override
     public double getPrice() {
         return 0;
     }
-    public void addIngrs(WeightedIngredient ingr){
+
+    public void addIngrs(WeightedIngredient ingr) {
         ingrs.add(ingr);
     }
-    public void removeIngrs(WeightedIngredient ingr){
+
+    public void removeIngrs(WeightedIngredient ingr) {
         ingrs.remove(ingr);
+    }
+
+    public void getScaledRecipe(Recipe recept, double x) {
+
     }
 }
